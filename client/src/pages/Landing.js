31826e1dfb9e30536125b810dccdf18f50b2d866
Adarsh Yadav <ay144344@gmail.com>
2025-12-14 01:10:21 +0530

    Render ready development

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,32 @@
+import main from '../assets/images/main.jpeg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Track your job applications, interviews, and offers in one place.
+            Create an account to add jobs, update statuses, and view progress
+            with helpful filters and statistics to stay organized and focused.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
