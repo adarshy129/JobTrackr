e1a28495cd9d7926c0a6eabce86d34004d729506
Adarsh Yadav <137261587+adarshy129@users.noreply.github.com>
2025-12-14 16:05:55 +0530

    Update logo image source to logo(1).jpg

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,4 +1,4 @@
-import logo from '../assets/images/logo.jpeg';
+import logo from '../assets/images/logo(1).jpg';
 
 const Logo = () => {
   return <img src={logo} alt='jobster logo' className='logo' />;
